1b8f6d705b44ae187d63f999a980c48cf0010c93
saran <saran@corpy.co.jp>
2026-01-07 22:13:05 +0900

    feat: Move alembic migrations to api/alembic

--- src/api/alembic/env.py
+++ src/api/alembic/env.py
@@ -1,10 +1,10 @@
 from logging.config import fileConfig
 
+from alembic import context
 from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
-from alembic import context
 from api.models.base import Base
 from config.settings import settings
 
