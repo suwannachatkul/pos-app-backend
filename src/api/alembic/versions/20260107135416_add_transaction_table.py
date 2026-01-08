d4c6ee5e2f6a3690c5149f5ec9efbeb728a33591
saran <saran@corpy.co.jp>
2026-01-08 22:15:43 +0900

    feat: fix lint

--- src/api/alembic/versions/20260107135416_add_transaction_table.py
+++ src/api/alembic/versions/20260107135416_add_transaction_table.py
@@ -6,9 +6,8 @@ Create Date: 2026-01-07 13:54:16.395497+00:00
 """
 
 import sqlalchemy as sa
-from sqlalchemy.dialects import postgresql
-
 from alembic import op
+from sqlalchemy.dialects import postgresql
 
 
 # revision identifiers, used by Alembic.
